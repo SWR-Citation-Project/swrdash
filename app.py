ff72599204d2156abe9744c83fc91888fff80ea2
Chris Lindgren <chris.a.lindgren@gmail.com>
2023-01-20 15:56:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ app.layout = dbc.Container(
 )
 
 if __name__ == "__main__":
-    # app.run_server(debug=True)
-    app.run_server(
-        host='0.0.0.0',
-        port=8080,
-        debug=False,
-        use_reloader=False)
+    app.run_server(debug=True)
+    # app.run_server(
+    #     host='0.0.0.0',
+    #     port=8080,
+    #     debug=False,
+    #     use_reloader=False)
